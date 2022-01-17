2195abc74d49ec3ba87c452589a3fc709398406d
Sivakar Sithamparanathan <sivakar12@outlook.com>
2022-01-17 11:30:49 +0530

    Add basic state and display functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+type LetterStatus = 
+  'guessedWrongPosition' | 'guessedRightPosition' | 'guessedWrong' | 'notGuessed';
+
+function colorForLetter(letterStatus: LetterStatus) {
+  switch (letterStatus) {
+    case 'guessedWrongPosition':
+      return 'yellow';
+    case 'guessedRightPosition':
+      return 'green';
+    case 'guessedWrong':
+      return 'grey';
+    case 'notGuessed':
+      return '';
+  }
+}
+type LetterStatusMap = {
+  [key: string]: LetterStatus;
+}
+
+const initialLetterStatus: LetterStatusMap = 
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .reduce((acc, letter) => (
+      {letter: 'notGuessed', ...acc}), 
+      {}
+    );
+
+type Attempts = string[];
+
+const WORD_LENGT = 5;
+const MAX_ATTEMPTS = 6;
+
+function displayWord(word: string, letterStatus: LetterStatusMap) {
+  return (
+    <div>
+      {
+        word.split('').map(letter => {
+          const color = colorForLetter(letterStatus[letter]);
+          return (
+            <span style={{color: color}}>
+              {letter}
+            </span>
+          );
+        }) 
+      } 
+    </div>
+  );
+}
 
 function App() {
+
+  const [word, setWord] = React.useState('APPLE');
+  const [currentEntry, setCurrentEntry] = React.useState('');
+  const [attempts, setAttempts] = React.useState<Attempts>(['TOTAL', 'WATER', 'INDIA']);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>WORDLE</div>
+      <div>
+        {attempts.map(attempt => displayWord(attempt, initialLetterStatus))}
+      </div>
     </div>
   );
 }
